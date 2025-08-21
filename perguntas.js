28f77a0a8bb763751c6cf27f213f85a5e1b91e4a
Amabilli1211 <Amabilli.procek@escola.pr.gov.br>
2025-08-21 10:42:38 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -1,1 +1,23 @@
+criaCartao(
+  'Paleontologia',
+  'Qual é o maior dinossauro carnívoro?',
+  'O maior dinossauro carnívoro é o Spinosaurus'
+)
 
+criaCartao(
+  'Paleontologia',
+  'Qual dinossauro é conhecido pelo seu pescoço longo?',
+  'O Brachiosaurus'
+)
+
+criaCartao(
+  'Paleontologia',
+  'O Tyrannosaurus Rex era herbívoro ou carnívoro?',
+  'Ele era carnívoro'
+)
+
+criaCartao(
+  'Paleontologia',
+  'O que significa o nome Triceratops?',
+  'Significa "face com três chifres"'
+)
